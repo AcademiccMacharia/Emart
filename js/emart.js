3092c02208050842c4d982e25dc59adeacba0a01
AcademiccMacharia <machariab19@gmail.com>
2023-05-21 20:53:45 +0300

    [Update]: color manipulation

--- js/emart.js
+++ js/emart.js
@@ -43,11 +43,19 @@ window.addEventListener('scroll', () => {
 
 let cartCount = 0;
 let cartCountElement = document.querySelector('.cart-count');
+let cartItems = [];
 
-function addToCart(count) {
-    cartCount = count;
-    cartCountElement.innerHTML = cartCount;
+function addToCart(product) {
+  cartCount++;
+  cartCountElement.innerHTML = cartCount;
+
+  // Add the product to the cart items array
+  cartItems.push(product);
+
+  // Update the cart card with the added product
+  updateCartCard();
 }
+
 // List all products
 
 const products_container = document.querySelector(".products-content");
@@ -60,28 +68,112 @@ function createProductCard(product) {
     title_product.innerText = product.title;
 
     const category = document.createElement("p");
-    title_product.innerText = product.category;
+    category.innerText = product.category;
 
     const price = document.createElement("p");
     price.innerHTML = `Price: <span>$${product.price}</span>`;
 
-    const button = document.createElement("button")
-    button.innerText = "Add to cart"
+    const viewButton = document.createElement("button");
+    viewButton.innerText = "View Product";
+    viewButton.addEventListener("click", () => {
+        displayProduct(product);
+    });
 
-    // button.addEventListener("click", () => {
-    //     addToCart(product)
-    // })
+    const addButton = document.createElement("button");
+    addButton.innerText = "Add to cart";
+    addButton.addEventListener("click", () => {
+        addToCart(product);
+    });
 
     const product_card = document.createElement("div");
     product_card.classList.add("product");
 
 
 
-    product_card.append(img, title_product, category, price, button);
+    product_card.append(img, title_product, category, price, viewButton, addButton);
 
     return product_card
 }
-//list all products
+
+
+
+function displayProduct(product) {
+    // Create the elements to display the product details
+    const productContainer = document.createElement("div");
+    productContainer.classList.add("product-page");
+
+    const productImage = document.createElement("img");
+    productImage.setAttribute("src", product.image);
+
+    const productTitle = document.createElement("h2");
+    productTitle.innerText = product.title;
+
+    const productCategory = document.createElement("p");
+    productCategory.innerText = product.category;
+
+    const productPrice = document.createElement("p");
+    productPrice.innerHTML = `Price: <span>$${product.price}</span>`;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "Go back";
+    backButton.addEventListener("click", () => {
+        mountProducts();
+        productContainer.innerHTML = "";
+    });
+
+    // Append the elements to the product container
+    productContainer.append(productImage, productTitle, productCategory, productPrice, backButton);
+
+    // Replace the products container with the product page
+    products_container.innerHTML = "";
+    products_container.appendChild(productContainer);
+}
+
+function updateCartCard(e) {
+    e.preventDefault();
+    const cartCard = document.querySelector(".cart-card");
+    const cartContent = document.querySelector(".cart-card .content");
+  
+    // Clear existing items in the cart card
+    cartContent.innerHTML = "";
+  
+    // Iterate through the cart items and add them to the cart card
+    cartItems.forEach((item) => {
+      const cartItem = document.createElement("div");
+      cartItem.classList.add("box");
+  
+      const removeIcon = document.createElement("i");
+      removeIcon.classList.add("fas", "fa-trash");
+  
+      const itemImage = document.createElement("img");
+      itemImage.setAttribute("src", item.image);
+  
+      const itemContent = document.createElement("div");
+      itemContent.classList.add("content");
+  
+      const itemTitle = document.createElement("h3");
+      itemTitle.innerText = item.title;
+  
+      const itemPrice = document.createElement("span");
+      itemPrice.classList.add("price");
+      itemPrice.innerText = `Price: $${item.price}`;
+  
+      const itemQuantity = document.createElement("span");
+      itemQuantity.classList.add("quantity");
+      itemQuantity.innerText = "Quantity: 1";
+  
+      itemContent.append(itemTitle, itemPrice, itemQuantity);
+      cartItem.append(removeIcon, itemImage, itemContent);
+      cartContent.appendChild(cartItem);
+    });
+  }
+  
+  
+
+
+
+
+//fetch api
 const product_url = "https://fakestoreapi.com/products";
 
 async function getAllProducts() {
